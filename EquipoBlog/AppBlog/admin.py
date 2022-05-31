8835547377258277a290abd4105fc52c377888d2
AbigailLiempe <kaminarirose@gmail.com>
2022-05-30 22:07:10 -0300

    registro

--- EquipoBlog/AppBlog/admin.py
+++ EquipoBlog/AppBlog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Equipo)
+admin.site.register(Lider)
+admin.site.register(Publicaciones)
+admin.site.register(Colaborador)
+admin.site.register(Avatar)
--- EquipoBlog/EquipoBlog/urls.py
+++ EquipoBlog/EquipoBlog/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppBlog/', include('AppBlog.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
