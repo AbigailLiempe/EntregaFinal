e253dda7de97f3ef4b52744921e6c70736bf021f
AbigailLiempe <kaminarirose@gmail.com>
2022-05-30 22:58:40 -0300

    manageando el desastre

--- EquipoBlog/EquipoBlog/urls.py
+++ EquipoBlog/EquipoBlog/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppBlog/', include('AppBlog.urls'))
 ]
+
+
+
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
