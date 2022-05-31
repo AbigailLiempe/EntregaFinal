c555d2226c07581bee237c6ff0f08186f28c2670
AbigailLiempe <kaminarirose@gmail.com>
2022-05-30 22:47:10 -0300

    Actualizando imagenes

--- EquipoBlog/AppBlog/admin.py
+++ EquipoBlog/AppBlog/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
+
+from AppBlog.views import about
 from .models import *
 
 # Register your models here.
@@ -11,4 +13,3 @@ admin.site.register(Lider)
 admin.site.register(Publicaciones)
 admin.site.register(Colaborador)
 admin.site.register(Avatar)
-admin.site.register(About)
